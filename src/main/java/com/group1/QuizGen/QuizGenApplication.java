aec6197c4c36a74c7d46811e77f121d35e776864
Sayem <imtiaz.cuet@gmail.com>
2017-10-18 13:02:47 -0500

    Project initialized

---
+++ src/main/java/com/group1/QuizGen/QuizGenApplication.java
@@ -0,0 +1,12 @@
+package com.group1.QuizGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGenApplication.class, args);
+	}
+}
