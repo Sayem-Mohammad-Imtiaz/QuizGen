4b4a347e07749a6063f04d374fe7863247532fe8
doctorwheauxdat <alexdodd@bellsouth.net>
2017-10-25 17:30:08 -0500

    early chapter select
    added a simple first step for chapter selection.

---
+++ src/main/resources/static/js/chapter_select.js
@@ -0,0 +1,30 @@
+$(document.body).on('mousedown', '#cb1', function(event){
+	chapter1_selected(event);
+})
+
+function chapter1_selected(event) {
+	console.log("log");
+    if ($("#ch2").is(':visible')) {
+        $("#ch2").css("display", 'none');
+    }
+    else {
+        $("#ch2").show();
+    }
+}
+$(document.body).on('mousedown', '#cb2', function(event){
+	chapter2_selected(event);
+})
+
+function chapter2_selected(event) {
+	console.log("log");
+    if ($("#ch3").is(':visible')) {
+        $("#ch3").css("display", 'none');
+    }
+    else {
+        $("#ch3").show();
+    }
+}
+
+function chapter_generate(){
+
+}
