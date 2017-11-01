52cf4a1e803a28f45d1a1b7ec9ec79877203bb7f
Sayem <imtiaz.cuet@gmail.com>
2017-11-01 03:04:48 -0500

    rest request with parameter sample added

--- src/main/java/com/group1/QuizGen/controller/rest/QuizController.java
+++ src/main/java/com/group1/QuizGen/controller/rest/QuizController.java
@@ -3,6 +3,7 @@ package com.group1.QuizGen.controller.rest;
 import com.group1.QuizGen.model.Quiz;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
 
     @GetMapping("/")
-    public Quiz getQuiz()
+    public Quiz getQuiz(@RequestParam Integer questionNo)
     {
         Quiz q=new Quiz();
-        q.setQuizName("Test Quiz");
+        q.setQuizName("You have requested "+questionNo+" questions");
         return q;
     }
 }
--- src/main/resources/static/js/quizcreator.js
+++ src/main/resources/static/js/quizcreator.js
@@ -3,6 +3,7 @@ function getQuiz()
     $.ajax({
         url: "/quizgen/rest/quiz/",
         type: 'get',
+        data: {questionNo: 2},
         success: function( data ) {
             $("#content").html(data.quizName);
         }
