80081a2e4a69c2b88f908635d182b03034171f24
Brandon Odom <girrig3@gmail.com>
2017-11-15 12:34:03 -0600

    Stats page colored bars

--- src/main/resources/static/js/statscreator.js
+++ src/main/resources/static/js/statscreator.js
@@ -11,6 +11,9 @@ function getStats()
     });
 }
 
+// Array of the correct answers
+var correct_answers = [1];
+
 
 // These will be related to the data variable eventually
 var questions = ["Question Text #1", "Question Text #2", "Question Text #3", "Question Text #4", "Question Text #5"];
@@ -28,6 +31,16 @@ for (var i=0; i<questions.length; i++) {
     for (var j=0; j<answers.length; j++){
         quiz_table.append("<tr id=\"quiz" + i + "-row" + j + "\"></tr>")
         quiz_table.append("<td>" + (j+1) + ") " + answers[j] + "</td><br>");
-        quiz_table.append("<td class=\"stats-bar-green\"> 100% </td>");
+        if (correct_answers[j] == (j+1)) {
+            quiz_table.append("<td> 100% <div class=\"stats-bar-green\" style=\"width:100px\"></div></td>");
+
+        } else {
+            quiz_table.append("<td> 100% <div class=\"stats-bar-red\" style=\"width:100px\"></div></td>");
+        }
+
     }
+    // Adds an Did Not Answer option
+    quiz_table.append("<tr id=\"quiz" + i + "-row" + 5 + "\"></tr>")
+    quiz_table.append("<td>5) Did Not Answer</td><br>");
+    quiz_table.append("<td> 100% <div class=\"stats-bar-grey\" style=\"width:100px\"></div></td>");
 }
