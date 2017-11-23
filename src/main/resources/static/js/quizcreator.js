dc07ed405ab911cfa1499c174b23f381f58829a0
Sayem <imtiaz.cuet@gmail.com>
2017-11-23 02:09:10 -0600

    full flow complete for taking new quiz

--- src/main/resources/static/js/quizcreator.js
+++ src/main/resources/static/js/quizcreator.js
@@ -50,22 +50,21 @@ function sendResults()
     });
 }
 
+function validateFirstForm(ref)
+{
 
-// These will be related to the data variable eventually
-// var questions = ["Question Text #1", "Question Text #2", "Question Text #3", "Question Text #4", "Question Text #5"]
-// var answers = ["Answer Text #1", "Answer Text #2", "Answer Text #3", "Answer Text #4"];
-// var correct_answers;
-//
-//
-// var quiz = $("#quiz");
-//
-// for (var i=0; i<questions.length; i++) {
-//     quiz.append("<div id=\"quiz-question" + i + "\" class=\"uk-card uk-card-default uk-card-body\"></div>");
-//     var quiz_div = $("#quiz-question" + i);
-//     quiz_div.append("<h3 class=\"uk-card-title\">" + questions[i] + "</h3>");
-//     quiz_div.append("<p id=\"quiz-para" + i + "\"></p>");
-//     var quiz_para = $("#quiz-para" + i);
-//     for (var j=0; j<answers.length; j++){
-//         quiz_para.append("<label><input class=\"uk-radio\" type=\"radio\" name=\"radio" + i + "\">  " + (j+1) + ") " + answers[j] + "</label><br>");
-//     }
-// }
+    var v=$("option:selected").val();
+    if(v==undefined || v==null ||v=='')
+    {
+        $('select').focus();
+        UIkit.modal.alert("Please choose chapter(s)!");
+        return false;
+    }
+    v=$("input[type='number']").val();
+    if(v==undefined || v==null ||v=='')
+    {
+        $('input').focus();
+        UIkit.modal.alert("Please enter number of questions!");
+        return false;
+    }
+}
